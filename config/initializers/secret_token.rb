ff1aeb95b18ef52e45d4c62744e83e6dcd0bb83b
Alex Corre <alexcorre513@gmail.com>
2012-03-11 17:28:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beartracks::Application.config.secret_token = 'c2d8397dcce66a4726225c4010bba611b675764eecab058f12d9cc2cae3d08a46ccf6df750ea871b4194821a97061d4f344919a00820a0d7e88d2b418972a5dc'
