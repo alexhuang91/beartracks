9e054dedb04ec8b80015dfbc062cefbd2b3ade11
Alex Corre <alexcorre513@gmail.com>
2012-03-11 18:39:04 -0700

    shitty front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Beartracks::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
+  root :to => 'application#home'
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
