eb2f184aa9ca6cf0791dc72f3f27c9dc3742c630
Alex Corre <alexcorre513@gmail.com>
2012-03-11 19:33:50 -0700

    removed the unneeded clerk model files

--- app/models/clerk.rb
+++
@@ -1,2 +0,0 @@
-class Clerk < ActiveRecord::Base
-end
--- db/migrate/20120312011923_create_clerks.rb
+++
@@ -1,8 +0,0 @@
-class CreateClerks < ActiveRecord::Migration
-  def change
-    create_table :clerks do |t|
-
-      t.timestamps
-    end
-  end
-end
