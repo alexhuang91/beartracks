f179897893e0ee68a9be2ea3312f1354e5dc792f
alex <alexhuang91@gmail.com>
2012-03-16 20:02:24 -0700

    Added models for user, resident, package. Also added fields to an initial migration. Have yet to add anything to the models files.

---
+++ db/migrate/001_initial_schema.rb
@@ -0,0 +1,38 @@
+class InitialSchema < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, :null => false, :unique => true
+      t.string :password
+      t.timestamps
+    end
+    create_table :packages do |t|
+      t.integer :tracking_number, :null => false, :unique => true
+      t.string :resident_name, :null => false
+      t.integer :resident_id
+      t.datetime :datetime_received, :null => false
+      t.string :carrier
+      t.string :unit, :null => false
+      t.string :building, :null => false
+      t.integer :room, :null => false
+      t.integer :clerk_received_id
+      t.integer :clerk_accepted_id
+      t.datetime :datetime_accepted
+      t.string :sender_address
+      t.string :sender_city
+      t.string :sender_state
+      t.integer :sender_zip
+      t.text :description
+      t.timestamps
+    end
+    create_table :residents do |t|
+      t.string :user_id
+      t.string :name
+      t.string :unit
+      t.string :building
+      t.integer :room
+      t.string :preference
+      t.integer :phone_number
+      t.string :email
+    end
+  end
+end
