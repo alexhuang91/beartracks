fd4bb4d925eb8e5707a69a10f543aee22e1e5467
Alex Corre <alexcorre513@gmail.com>
2012-03-19 15:10:52 -0700

    started pimping out the css for application.css and added a footer to the main layout main.html.haml

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,45 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 1) do
+
+  create_table "packages", :force => true do |t|
+    t.integer  "tracking_number",   :null => false
+    t.string   "resident_name",     :null => false
+    t.integer  "resident_id"
+    t.datetime "datetime_received", :null => false
+    t.string   "carrier"
+    t.string   "unit",              :null => false
+    t.string   "building",          :null => false
+    t.integer  "room",              :null => false
+    t.integer  "clerk_received_id"
+    t.integer  "clerk_accepted_id"
+    t.datetime "datetime_accepted"
+    t.string   "sender_address"
+    t.string   "sender_city"
+    t.string   "sender_state"
+    t.integer  "sender_zip"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "residents", :force => true do |t|
+    t.string  "user_id"
+    t.string  "name"
+    t.string  "unit"
+    t.string  "building"
+    t.integer "room"
+    t.string  "preference"
+    t.integer "phone_number"
+    t.string  "email"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username",   :null => false
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
