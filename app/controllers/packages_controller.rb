ab25893b71c074808b0a7da6db4f6f3d7acfd6c2
Case Walker <4casewalker@gmail.com>
2012-04-05 03:38:31 -0700

    Some modifications to package creation

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,64 +1,71 @@
 class PackagesController < ApplicationController
 
-def show
-  @package = Package.find params[:id]
-end
+  def show
+    @package = Package.find params[:id]
+  end
 
-def index
+  def index
 # need logic to resolve what unit's packages are shown, so as to stay restful
 # this can probably be done same as how ratings are remebered in movies_controller
-  @packages = Package.all
+    @packages = Package.all
 # @packages = Package.find_all_by_unit[params[:unit]]
-end
-
-def edit
-  @package = Package.find params[:id]
-end
-
-def new
-
-end
-
-def create
-  p = params[:package]
-  p[:datetime_received] = Time.now #TODO deal with timezone issue
-  p[:clerk_received_id] = current_clerk.id
-  if Package.create(p)
-    # todo Send out an email or text or add to the slip-queue
-    flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was created at #{p[:datetime_received]}"
-  else
-    flash[:error] = "There was an error creating this package."
   end
-# consider having two paths, one redirects to packages_path, the other to another new package
-  redirect_to packages_path
-end
 
-def update
-  @package = Package.find params[:id]
-  p = params[:package]
-  if @package.update_attributes(p)
-    flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was updated at #{p[:datetime_received]}"
-    redirect_to package_path params[:id]
-  else
-    flash[:error] = "There was an error in updating this package."
-    redirect_to edit_package_path params[:id]
+  def edit
+    @package = Package.find params[:id]
   end
-end
 
-def destroy
-
-end
-
-def picked_up
-  p = Package.find(params[:id])
-  p.clerk_accepted_id = current_clerk.id
-  p.datetime_accepted = Time.now
-  if p.save
-    flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was picked up at #{p[:datetime_received].time}"
-  else
-    flash[:error] = "There was an error in updating this package."
+  def new
   end
-  redirect_to packages_path
-end
 
+  def create
+    p = Package.new params[:package]
+    if not p.has_required_fields
+      flash[:error] = "Cannot leave #{p.blank_fields} blank"
+      redirect_to new_package_path
+    else
+      p[:datetime_received] = Time.now #TODO deal with timezone issue
+      p[:clerk_received_id] = current_clerk.id
+      if p.save
+      # todo Send out an email or text or add to the slip-queue
+        flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was created at #{p[:datetime_received]}"
+      else
+        flash[:error] = "There was an error creating this package."
+      end
+# now that the package is saved, redirect appropriately
+      if params[:commit] == "Create Package"
+        redirect_to packages_path
+      else
+        redirect_to new_package_path
+      end
+    end
+  end
+
+  def update
+    @package = Package.find params[:id]
+    p = params[:package]
+    if @package.update_attributes(p)
+      flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was updated at #{p[:datetime_received]}"
+      redirect_to package_path params[:id]
+    else
+      flash[:error] = "There was an error in updating this package."
+      redirect_to edit_package_path params[:id]
+    end
+  end
+
+  def destroy
+# what is there to do here, just remove it from the db? but who gets to do this, admins and/or clerks, and when?
+  end
+
+  def picked_up
+    p = Package.find(params[:id])
+    p.clerk_accepted_id = current_clerk.id
+    p.datetime_accepted = Time.now
+    if p.save
+      flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was picked up at #{p[:datetime_received].time}"
+    else
+      flash[:error] = "There was an error in updating this package."
+    end
+    redirect_to packages_path #redirect elsewhere?
+  end
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,24 @@
 class Package < ActiveRecord::Base
+
+  def unfilled_fields
+    @@never_null_fields = {:resident_name=>'Resident Name', :unit=>'Unit', :building=>'Building',
+      :room=>'Room',:tracking_number=>'Tracking Number'} #leaving datetime_received out, not for user
+    empty_fields = @@never_null_fields.keys.
+      collect{ |field| field if (not self[field] or self[field].to_s.gsub(/[\s]/,'')=="") }.
+      compact
+  end
+
+  def has_required_fields
+    self.unfilled_fields.length == 0
+  end
+
+  def blank_fields
+    return "(nothing left blank, bad usage)" if self.has_required_fields
+    fields = self.unfilled_fields
+    field_strings = fields.collect { |f| @@never_null_fields[f] }
+    if fields.length > 1
+      field_strings[-1] = 'or '+field_strings[-1]
+    end
+    string = field_strings.join(', ')
+  end
 end
