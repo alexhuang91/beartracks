5db6ea7f646f1464bbc3b18398021756d7ba0fad
Case Walker <4casewalker@gmail.com>
2012-04-05 20:47:44 -0700

    added methods units_array and states_array that returns their appropriate string arrays, fixed the time-bug, pulled new and edit views mostly into one partial, did some work on the controller, made it so only non-picked up packages show up on the package page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,42 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   helper_method :current_clerk_session, :current_clerk
   helper_method :current_resident_session, :current_resident
+  helper_method :units_array, :states_array
   private
-    
+
     def current_clerk_session
       return @current_clerk_session if defined?(@current_clerk_session)
       @current_clerk_session = ClerkSession.find
     end
-    
+
     def current_clerk
       return @current_clerk if defined?(@current_clerk)
       @current_clerk = current_clerk_session && current_clerk_session.clerk
     end
-    
+
     def current_resident_session
       return @current_resident_session if defined?(@current_resident_session)
       @current_resident_session = ResidentSession.find
     end
-    
+
     def current_resident
       return @current_resident if defined?(@current_resident)
       @current_resident = current_resident_session && current_resident_session.resident
     end
 
+    def units_array
+      ['Unit 1', 'Unit 2', 'Unit 3', 'Foothill', 'Bowles', 'Stern',
+      'Clark Kerr', 'Manville', 'Ida L. Jackson', 'Channing-Bowditch',
+      'Martinez Commons', 'University Village']
+    end
+
+    def states_array
+      ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+      "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+      "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+      "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+      "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    end
 end
--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,20 +2,36 @@ class PackagesController < ApplicationController
 
   def show
     @package = Package.find params[:id]
+    @clerk_received = Clerk.find @package.clerk_received_id
+    if @package.clerk_accepted_id
+      @accepted = true
+      @clerk_released = Clerk.find @package.clerk_accepted_id
+    end
   end
 
   def index
 # need logic to resolve what unit's packages are shown, so as to stay restful
 # this can probably be done same as how ratings are remebered in movies_controller
-    @packages = Package.all
-# @packages = Package.find_all_by_unit[params[:unit]]
+#   if params[:unit] == current_clerk.unit
+#     if params[:unit] == 'all'
+#       @packages = Package.where :datetime_accepted => nil
+#     else
+        @packages = Package.where :datetime_accepted => nil #, :unit => params[:unit]
+#     end
+#   else
+#     current_clerk.unit = params[:unit]
+#     flash.keep
+#     redirect_to packages_path
+#   end
   end
 
   def edit
+    @new = false
     @package = Package.find params[:id]
   end
 
   def new
+    @new = true
   end
 
   def create
@@ -24,11 +40,11 @@ class PackagesController < ApplicationController
       flash[:error] = "Cannot leave #{p.blank_fields} blank"
       redirect_to new_package_path
     else
-      p[:datetime_received] = Time.now #TODO deal with timezone issue
+      p[:datetime_received] = Time.now.to_datetime
       p[:clerk_received_id] = current_clerk.id
       if p.save
       # todo Send out an email or text or add to the slip-queue
-        flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was created at #{p[:datetime_received]}"
+        flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was created at #{p[:datetime_received].localtime.ctime}"
       else
         flash[:error] = "There was an error creating this package."
       end
@@ -44,8 +60,9 @@ class PackagesController < ApplicationController
   def update
     @package = Package.find params[:id]
     p = params[:package]
+    p[:updated_at] = Time.now.to_datetime
     if @package.update_attributes(p)
-      flash[:notice] = "package #{@package.tracking_number} for #{@package.resident_name} was updated at #{Time.now.ctime}}"
+      flash[:notice] = "package #{@package.tracking_number} for #{@package.resident_name} was updated at #{p.updated_at.localtime.ctime}}"
       redirect_to package_path @package
     else
       flash[:error] = "There was an error in updating this package."
@@ -60,9 +77,9 @@ class PackagesController < ApplicationController
   def picked_up
     p = Package.find(params[:id])
     p.clerk_accepted_id = current_clerk.id
-    p.datetime_accepted = Time.now
+    p.datetime_accepted = Time.now.to_datetime
     if p.save
-      flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was picked up at #{p[:datetime_received].time}"
+      flash[:notice] = "package #{p[:tracking_number]} for #{p[:resident_name]} was picked up at #{p[:datetime_received].localtime.ctime}"
     else
       flash[:error] = "There was an error in updating this package."
     end
