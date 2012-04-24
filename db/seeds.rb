6aa3788a39a301f97fd607160a0bbe5457b58b54
Case Walker <4casewalker@gmail.com>
2012-04-24 00:19:08 -0700

    added three clerks to be seeded, 'admin' for us and our customers, 'cs169_admin' and 'cs169_clerk' for the graders. do 'rake db:seed' to put them in your db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+#   create an admin to be the base admin in the system for the dorm people
+#   create another admin to be what our class uses for grading
+#
+  first_admin = Clerk.create( first_name: 'Master', last_name: 'Admin', login: 'admin',
+      unit: 'Unit 1', password: 'pass', password_confirmation: 'pass', email: 'a@a.com' )
+  first_admin.is_admin = true
+  first_admin.save
+
+  class_admin = Clerk.create( first_name: 'Armando', last_name: 'Foxxymama', login: 'cs169_admin',
+      unit: 'Unit 3', password: 'pass', password_confirmation: 'pass', email: 'b@b.com' )
+  class_admin.is_admin = true
+  class_admin.save
+
+  class_clerk = Clerk.create( first_name: 'David', last_name: 'Pattypatpat', login: 'cs169_clerk',
+      unit: 'Unit 3', password: 'pass', password_confirmation: 'pass', email: 'c@c.com' )
