3a54cc647cf040361788b5c41e0199ce3d4250bc
Case Walker <4casewalker@gmail.com>
2012-05-01 15:02:34 -0700

    Now we are using arrays instead of hashes for units and buildings. working on making dynamic select menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_clerk_session, :current_clerk, :clerk_logged_in?
   helper_method :current_resident_session, :current_resident
   helper_method :preferences_array, :states_array, :carriers_array, :package_type_array
-  helper_method :units_hash, :buildings_hash
+  helper_method :units_array, :buildings_arrays, :all_buildings
   
   private
     
@@ -58,94 +58,103 @@ class ApplicationController < ActionController::Base
       ["Tube", "Box", "Large box", "Envelope", "Letter", "Other"]
     end
 
-    def units_hash
-      {"Unit 1"             => "Unit 1",
-       "Unit 2"             => "Unit 2",
-       "Unit 3"             => "Unit 3",
-       "Unit 4"             => "Unit 4",
-       "Clark Kerr"         => "Clark Kerr",
-       "University Village" => "University Village",
-       "Smyth Fernwald"     => "Smyth Fernwald"}
+    def units_array
+      ["Unit 1",
+       "Unit 2",
+       "Unit 3",
+       "Unit 4",
+       "Clark Kerr",
+       "Smyth Fernwald",
+       "University Village"]
     end
 
-    def buildings_hash(unit)
-      if(unit == "CKC")
-        hash = {"Building 1"  => "Building 1",
-                "Building 2"  => "Building 2",
-                "Building 3"  => "Building 3",
-                "Building 4"  => "Building 4",
-                "Building 6"  => "Building 6",
-								"Building 5"  => "Building 5",
-								"Building 7"  => "Building 7",
-								"Building 8"  => "Building 8",
-                "Building 9"  => "Building 9",
-								"Building 11" => "Building 11",
-								"Building 12" => "Building 12",
-								"Building 13" => "Building 13",
-                "Building 14" => "Building 14",
-								"Building 15" => "Building 15",
-								"Building 16" => "Building 16",
-								"Building 17" => "Building 17",
-                "Building 18" => "Building 18",
-								"Building 19" => "Building 19",
-								"Building 20" => "Building 20"}
+    def buildings_arrays(unit)
+      units = units_array
 
-      elsif(unit == "U1")
-        hash = {"Channing Bowditch" => "Channing Bowditch",
-								"Cheney Hall"       => "Cheney Hall",
-								"Christian Hall"    => "Christian Hall",
-								"Deutsch Hall"      => "Deutsch Hall",
-                "Freeborn Hall"     => "Freeborn Hall",
-								"Ida Jackson House" => "Ida Jackson House",
-								"Putnam Hall"       => "Putnam Hall",
-								"Slottman Hall"     => "Slottman Hall"}
+      if(unit == units[0]) # Unit 1
+        array = ["Channing Bowditch",
+								 "Cheney Hall",
+								 "Christian Hall",
+								 "Deutsch Hall",
+                 "Freeborn Hall",
+								 "Ida Jackson House",
+								 "Putnam Hall",
+								 "Slottman Hall"]
 
-      elsif(unit == "U2")
-        hash = {"Cunningham Hall" => "Cunningham Hall",
-								"Davidson Hall"   => "Davidson Hall",
-								"Ehman Hall"      => "Ehman Hall",
-								"Griffiths Hall"  => "Griffiths Hall",
-								"Towle Hall"      => "Towle Hall",
-								"Wada Hall"       => "Wada Hall"}
+      elsif(unit == units[1]) # Unit 2
+        array = ["Cunningham Hall",
+								 "Davidson Hall",
+								 "Ehman Hall",
+								 "Griffiths Hall",
+								 "Towle Hall",
+								 "Wada Hall"]
 
-      elsif(unit == "U3")
-        hash = {"Beverly Cleary Hall" => "Beverly Cleary Hall",
-								"Ida Sproul Hall"     => "Ida Sproul Hall",
-								"Manville Hall"       => "Manville Hall",
-								"Norton Hall"         => "Norton Hall",
-								"Priestley Hall"      => "Priestley Hall",
-								"Spens Black Hall"    => "Spens Black Hall"}
+      elsif(unit == units[2]) # Unit 3
+        array = ["Beverly Cleary Hall",
+								 "Ida Sproul Hall",
+								 "Manville Hall",
+								 "Norton Hall",
+								 "Priestley Hall",
+								 "Spens Black Hall"]
 
-      elsif(unit == "U4")
-        hash = {"FH Building 1" => "FH Building 1",
-								"FH Building 2" => "FH Building 2",
-								"FH Building 3" => "FH Building 3",
-								"FH Building 4" => "FH Building 4",
-								"FH Building 5" => "FH Building 5",
-								"FH Building 6" => "FH Building 6",
-								"FH Building 7" => "FH Building 7",
-								"FH Building 8" => "FH Building 8",
-								"FH Building 9" => "FH Building 9",
-								"Bowles Hall"   => "Bowles Hall",
-								"Stern Hall"    => "Stern Hall"}
+      elsif(unit == units[3]) # Unit 4
+        array = ["FH Building 1",
+								 "FH Building 2",
+								 "FH Building 3",
+								 "FH Building 4",
+								 "FH Building 5",
+								 "FH Building 6",
+								 "FH Building 7",
+								 "FH Building 8",
+								 "FH Building 9",
+								 "Bowles Hall",
+								 "Stern Hall"]
 
-      elsif(unit == "UV")
-        hash = {"East" => "East",
-                "West" => "West"}
+      elsif(unit == units[4]) # Clark Ker
+        array = ["Building 1",
+                 "Building 2",
+                 "Building 3",
+                 "Building 4",
+                 "Building 6",
+								 "Building 5",
+								 "Building 7",
+								 "Building 8",
+                 "Building 9",
+								 "Building 11",
+								 "Building 12",
+								 "Building 13",
+                 "Building 14",
+								 "Building 15",
+								 "Building 16",
+								 "Building 17",
+                 "Building 18",
+								 "Building 19",
+								 "Building 20"]
 
-      elsif(unit == "SF")
-        hash = {"3002" => "3002",
-                "3024" => "3024",
-                "E" => "E",
-                "F" => "F",
-                "G" => "G",
-                "H" => "H",
-                "J" => "J",
-                "K" => "K",
-                "L" => "L"}
+      elsif(unit == units[5]) # University Village
+        array = ["East",
+                 "West"]
+
+      elsif(unit == units[6]) # Smyth Fernwald
+        array = ["3002",
+                 "3024",
+                 "E",
+                 "F",
+                 "G",
+                 "H",
+                 "J",
+                 "K",
+                 "L"]
       else
-        hash = {}
+        array = []
       end
-      hash
+      array
+    end
+
+    def all_buildings
+      buildings = []
+      units = units_array
+      units.each { |unit| buildings << buildings_arrays(unit) }
+      buldings.flatten
     end
 end
