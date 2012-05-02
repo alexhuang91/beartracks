6befdfbf84a17b99524f9a94a02b85ec3162cec1
Alex Corre <alexcorre513@gmail.com>
2012-05-02 16:28:28 -0700

    starting email verificaton + added migration for verified boolean column in clerks.

--- app/models/clerk_session.rb
+++ app/models/clerk_session.rb
@@ -1,2 +1,11 @@
 class ClerkSession < Authlogic::Session::Base
+
+  validate :check_if_verified
+  
+  private
+  
+  def check_if_verified
+    errors.add(:base, "Your account has not yet been verified") unless attempted_record and attempted_record.verified
+  end
+
 end
